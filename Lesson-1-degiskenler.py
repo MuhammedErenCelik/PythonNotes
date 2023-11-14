58ac56561d33b172fb964a71618c09453c37f118
MuhammedErenCelik <149884984+MuhammedErenCelik@users.noreply.github.com>
2023-11-14 16:32:32 +0300

    Add files via upload

---
+++ Lesson-1-degiskenler.py
@@ -0,0 +1,10 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
