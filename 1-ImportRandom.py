37bd68c0a71d124228f14c7eda9ea6b2bd717536
MuhammedErenCelik <149884984+MuhammedErenCelik@users.noreply.github.com>
2023-11-14 17:25:14 +0300

    Add files via upload

---
+++ 1-ImportRandom.py
@@ -0,0 +1,10 @@
+import random
+
+UretilenSayi1=random.randint(10,20)
+
+UretilenSayi2=random.randint(11,19)
+
+UretilenSayi1>UretilenSayi2
+print("Birinci sayi: ",UretilenSayi1," ikinci sayi ",UretilenSayi2)
+
+print(UretilenSayi1>UretilenSayi2)
