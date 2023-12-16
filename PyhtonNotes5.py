cffe2346bd3779e80b9e17039ff11063d47f74d3
MuhammedErenCelik <149884984+MuhammedErenCelik@users.noreply.github.com>
2023-12-16 15:59:38 +0300

    Add files via upload

---
+++ PyhtonNotes5.py
@@ -0,0 +1,13 @@
+sayi1=int(input("1. notu giriniz"))
+
+sayi2=int(input("2. notu giriniz"))
+
+sayi3=int(input("3.notu giriniz"))
+
+ortalama=(sayi1+sayi2+sayi3)/3
+
+ortalama=round(ortalama,2)
+
+print(ortalama)
+
+
