eb58882ec34dc420119c859c89e9ed8f0624f1f1
MuhammedErenCelik <149884984+MuhammedErenCelik@users.noreply.github.com>
2024-08-03 15:50:18 +0300

    Update PyhtonNotes5.py

--- PyhtonNotes5.py
+++ PyhtonNotes5.py
@@ -1,8 +1,8 @@
-sayi1=int(input("1. notu giriniz"))
+sayi1=int(input("1. notu giriniz:"))
 
-sayi2=int(input("2. notu giriniz"))
+sayi2=int(input("2. notu giriniz:"))
 
-sayi3=int(input("3.notu giriniz"))
+sayi3=int(input("3.notu giriniz:"))
 
 ortalama=(sayi1+sayi2+sayi3)/3
 
